af7fc31342bea178c5b360894bb086a00bab937f
sundong <sundong@yingu.com>
2017-03-30 16:08:34 +0800

    备份一下

--- app/src/main/java/com/lewish/start/downloadmanagerdemo/DownloadActivity.java
+++ app/src/main/java/com/lewish/start/downloadmanagerdemo/DownloadActivity.java
@@ -1,7 +1,10 @@
 package com.lewish.start.downloadmanagerdemo;
 
 import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,19 +16,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class DownloadActivity extends AppCompatActivity {
+    public static final String DOWNLOAD_URL = "http://ucdl.25pp.com/fs08/2017/01/20/2/2_87a290b5f041a8b512f0bc51595f839a.apk";
+    public static final String FILE_NAME = "testApp.apk";
+    public static final String NOTIFICATION_TITLE = "大象投教";
+    public static final String NOTIFICATION_DESC = "一个坑爹的app";
+
+    private DownloadBroadcastReceiver mDownloadBroadcastReceiver;
     private TextView mTvFileName;
     private TextView mTvProgress;
     private ProgressBar mPbUpdate;
-    private Button mBtnStart;
 
+    private Button mBtnStart;
     private DownloadManager mDownloadManager;
     private DownloadManager.Request mDownloadManagerRequest;
-    public static final String DOWNLOAD_URL = "http://ucdl.25pp.com/fs08/2017/01/20/2/2_87a290b5f041a8b512f0bc51595f839a.apk";
+
     private Timer mTimer;
     private TimerTask mTimerTask;
     private long mDownLoadID;
@@ -48,7 +58,7 @@ public class DownloadActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initViews();
-
+        registBroadcastReceiver();
         initDownloadManager();
     }
 
@@ -57,8 +67,8 @@ public class DownloadActivity extends AppCompatActivity {
         mDownloadManagerRequest = new DownloadManager.Request(Uri.parse(DOWNLOAD_URL));
 
         //设置Notification标题和描述
-        mDownloadManagerRequest.setTitle("标题");
-        mDownloadManagerRequest.setDescription("描述");
+        mDownloadManagerRequest.setTitle(NOTIFICATION_TITLE);
+        mDownloadManagerRequest.setDescription(NOTIFICATION_DESC);
         mDownloadManagerRequest.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
 
         //指定网络下载类型
@@ -75,7 +85,7 @@ public class DownloadActivity extends AppCompatActivity {
 
         //创建目录
         Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).mkdir();
-        mDownloadManagerRequest.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "testApp.apk");
+        mDownloadManagerRequest.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, FILE_NAME);
 
         final DownloadManager.Query query = new DownloadManager.Query();
         mTimer = new Timer();
@@ -85,7 +95,7 @@ public class DownloadActivity extends AppCompatActivity {
                 if (cursor != null && cursor.moveToFirst()) {
                     if (cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)) == DownloadManager.STATUS_SUCCESSFUL) {
                         mPbUpdate.setProgress(100);
-                        install(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/testApp.apk");
+                        install(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/"+FILE_NAME);
                         mTimerTask.cancel();
                     }
                     String title = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_TITLE));
@@ -125,5 +135,29 @@ public class DownloadActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setDataAndType(Uri.parse("file://" + path), "application/vnd.android.package-archive");
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);//4.0以上系统弹出安装成功打开界面 startActivity(intent); }
+    }
+
+    public void registBroadcastReceiver(){
+        mDownloadBroadcastReceiver = new DownloadBroadcastReceiver();
+        IntentFilter intentFilter = new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        registerReceiver(mDownloadBroadcastReceiver,intentFilter);
+    }
+
+    public void unRegistBroadcastReceiver(){
+        unregisterReceiver(mDownloadBroadcastReceiver);
+        mDownloadBroadcastReceiver = null;
+    }
+
+    @Override
+    protected void onDestroy() {
+        unRegistBroadcastReceiver();
+        super.onDestroy();
+    }
+
+    public class DownloadBroadcastReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(DownloadActivity.this, "下载完成", Toast.LENGTH_SHORT).show();
+        }
     }
 }
