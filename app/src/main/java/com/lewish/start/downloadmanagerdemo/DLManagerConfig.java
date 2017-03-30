0ebd21bf369afc95523cbfe9ec18c159d0b84793
Lewish <LewishStart@163.com>
2017-03-30 22:11:01 +0800

    遗漏代码提交

---
+++ app/src/main/java/com/lewish/start/downloadmanagerdemo/DLManagerConfig.java
@@ -0,0 +1,94 @@
+package com.lewish.start.downloadmanagerdemo;
+
+/**
+ * Created by Lewish on 2017/3/30.
+ */
+
+public class DLManagerConfig {
+
+    private static final int DEFAULT_QUERY_INTERVAL = 1000;
+
+    private String downloadUrl;
+    private String fileName;
+    private String notificationTitle;
+    private String notificationDesc;
+    private long queryInterval = DEFAULT_QUERY_INTERVAL;
+
+    public String getDownloadUrl() {
+        return downloadUrl;
+    }
+
+    public void setDownloadUrl(String downloadUrl) {
+        this.downloadUrl = downloadUrl;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getNotificationTitle() {
+        return notificationTitle;
+    }
+
+    public void setNotificationTitle(String notificationTitle) {
+        this.notificationTitle = notificationTitle;
+    }
+
+    public String getNotificationDesc() {
+        return notificationDesc;
+    }
+
+    public void setNotificationDesc(String notificationDesc) {
+        this.notificationDesc = notificationDesc;
+    }
+
+    public long getQueryInterval() {
+        return queryInterval;
+    }
+
+    public void setQueryInterval(long queryInterval) {
+        this.queryInterval = queryInterval;
+    }
+
+    public static class Builder {
+        private DLManagerConfig dlManagerConfig;
+
+        public Builder() {
+            dlManagerConfig = new DLManagerConfig();
+        }
+
+        public Builder downLoadUrl(String downLoadUrl){
+            dlManagerConfig.downloadUrl = downLoadUrl;
+            return this;
+        }
+
+        public Builder fileName(String fileName){
+            dlManagerConfig.fileName = fileName;
+            return this;
+        }
+
+        public Builder notificationTitle(String notificationTitle){
+            dlManagerConfig.notificationTitle = notificationTitle;
+            return this;
+        }
+
+        public Builder notificationDesc(String notificationDesc){
+            dlManagerConfig.notificationDesc = notificationDesc;
+            return this;
+        }
+
+        public Builder queryInterval(long queryInterval){
+            dlManagerConfig.queryInterval = queryInterval;
+            return this;
+        }
+
+        public DLManagerConfig build(){
+            return dlManagerConfig;
+        }
+
+    }
+}
